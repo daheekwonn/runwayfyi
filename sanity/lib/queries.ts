ffc3c4696391c818f82f5d7d445eb41d08c5e419
daheekwonn <summerxkwon@gmail.com>
2026-03-30 20:36:23 +0100

    fix duplicate articlesQuery

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,15 +1,5 @@
 // sanity/lib/queries.ts
 
-export const articlesQuery = `*[_type == "article"] | order(publishedAt desc) [0...4] {
-  _id,
-  title,
-  slug,
-  category,
-  season,
-  excerpt,
-  publishedAt,
-  "coverImage": coverImage.asset->url,
-}`
 
 export const featuredArticleQuery = `*[_type == "article" && featured == true] | order(publishedAt desc) [0] {
   _id,
